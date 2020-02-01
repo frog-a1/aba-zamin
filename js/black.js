d5ec5e92e85a652254d4395454d13124a31a576b
frog-a1 <froga1coming@gmail.com>
2020-02-01 20:45:48 +0200

    #אבאזמין

---
+++ js/black.js
@@ -0,0 +1,6 @@
+function black() {
+    let black_win = document.getElementById('black_win');
+    let body = document.getElementsByTagName('body')[0];
+    black_win.classList.add('black');
+    body.classList.add('black');
+}
